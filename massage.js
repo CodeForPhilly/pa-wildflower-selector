e5a028631043ff5f5934a16769210298d38eb9dc
Tom Boutell <tom@apostrophecms.com>
2021-08-10 08:00:57 -0400

    cleanup and sync tools

--- massage.js
+++ massage.js
@@ -1,17 +1,26 @@
 const db = require('./lib/db');
+const fs = require('fs');
+const path = require('path');
 
 go();
 
 async function go() {
   const { plants, close } = await db();
   const values = await plants.find().toArray();
-  const names = values.filter(plant => plant['Scientific Name'].match(/ [a-z]/)).map(plant => plant['Scientific Name']);
-  await plants.removeMany({
-    'Scientific Name': {
-      $in: names
-    }
-  });
-  process.exit(0);
+  // Remove wrongly cased plants
+  // const names = values.filter(plant => plant['Scientific Name'].match(/ [a-z]/)).map(plant => plant['Scientific Name']);
+  // await plants.removeMany({
+  //   'Scientific Name': {
+  //     $in: names
+  //   }
+  // });
+  // Rename wrongly-cased images
+  // const files = fs.readdirSync('./images');
+  // const plantList = await plants.find().toArray();
+  // files.filter(file => file.match(/ [a-z]/)).map(file => {
+  //   const plant = plantList.find(plant => plant['Scientific Name'].toLowerCase() === file.toLowerCase().replace(/\..*$/, ''));
+  //   fs.renameSync(`images/${file}`, `images/${plant['Scientific Name']}.jpg`);
+  // });
   for (const plant of values) {
     const months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
     let [ from, to ] = plant['Flowering Months'].split('–');
